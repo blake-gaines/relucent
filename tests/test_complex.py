8ef747037c7b6c1bca5991a1cec9499138d85adf
blake <blake@uconn.edu>
2026-01-19 12:24:28 -0500

    cleanup

--- tests/test_complex.py
+++ tests/test_complex.py
@@ -4,27 +4,11 @@ from relucent import Complex, set_seeds, get_mlp_model
 def test_bfs_one():
     set_seeds(0)
 
-    widths = [16, 64, 64, 64, 10]
-
-    model = get_mlp_model(widths=widths)
+    model = get_mlp_model(widths=[16, 64, 64, 64, 10])
 
     print("Model:\n", model)
 
     cplx = Complex(model)
-
-    # # breakpoint()
-
-    # start = torch.rand(widths[0])
-    # end = torch.rand(widths[0])
-
-    # start_poly = cplx.point2poly(start)
-    # end_poly = cplx.point2poly(end)
-
-    # print("Hamming:", (start_poly.bv != end_poly.bv).sum())
-    # path = cplx.hamming_astar(nworkers=64, start=start, end=end)
-    # print("Path:", len(path) - 1)
-    # print()
-    # print(path)
 
     cplx.bfs(max_polys=100)
 
